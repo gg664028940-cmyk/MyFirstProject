ed5cdad678b88571f42fcbc632b3323f6e90fe38
WR <664028940@qq.com>
2026-01-15 17:24:16 +0800

    本地修改

--- Main.java
+++ Main.java
@@ -1,1 +1,1 @@
-public class Main { public static void main(String[] args) { System.out.println("Hello from GitHub"); } }
+public class Main { public static void main(String[] args) { System.out.println("我是本地的代码"); } }
