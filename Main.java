39c78dd4794eb349767cee8654264e26ec9fda14
gg664028940-cmyk <gg664028940@gmail.com>
2026-01-15 17:22:31 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,1 @@
-public class Main { public static void main(String[] args) { System.out.println("Hello from GitHub"); } }
+public class Main { public static void main(String[] args) { System.out.println("我是 GitHub 上的代码"); } }
